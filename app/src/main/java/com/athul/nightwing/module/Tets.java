09be09a2e50f5c8e27c9fa35d4677798ed04e59e
Athu <athul@entri.me>
2017-08-17 16:36:00 +0530

    Code to change drawer icon

--- app/src/main/java/com/athul/nightwing/module/Tets.java
+++ app/src/main/java/com/athul/nightwing/module/Tets.java
@@ -1,19 +1,39 @@
 package com.athul.nightwing.module;
 
+import android.content.res.XModuleResources;
 import android.util.Log;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 /**
  * Created by athul on 17/8/17.
  */
 
-public class Tets implements IXposedHookLoadPackage{
+public class Tets implements IXposedHookLoadPackage,IXposedHookZygoteInit,IXposedHookInitPackageResources {
+
+    private static String MODULE_PATH = null;
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         XposedBridge.log("Loaded app: " + loadPackageParam.packageName);
-        Log.e("GOT","IGOTY");
+
+    }
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+
+        MODULE_PATH = startupParam.modulePath;
+
+    }
+
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam initPackageResourcesParam) throws Throwable {
+        XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, initPackageResourcesParam.res);
+        Utils.changeDrawerIcon(initPackageResourcesParam,modRes);
     }
 }
---
+++ app/src/main/java/com/athul/nightwing/module/Utils.java
@@ -0,0 +1,41 @@
+package com.athul.nightwing.module;
+
+import android.content.res.XModuleResources;
+
+import com.athul.nightwing.R;
+
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+
+/**
+ * Created by athul on 17/8/17.
+ */
+
+public class Utils {
+
+    public static void changeDrawerIcon(XC_InitPackageResources.InitPackageResourcesParam resparam, XModuleResources modRes) {
+
+        if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.android.launcher3") != 0) {
+            resparam.res.setReplacement("com.android.launcher3", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.android.launcher3", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.android.launcher") != 0) {
+            resparam.res.setReplacement("com.android.launcher", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.android.launcher", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.cyanogenmod.trebuchet") != 0) {
+            resparam.res.setReplacement("com.cyanogenmod.trebuchet", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.cyanogenmod.trebuchet", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.anddoes.launcher") != 0) {
+            resparam.res.setReplacement("com.anddoes.launcher", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.anddoes.launcher", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.google.android.googlequicksearchbox") != 0) {
+            resparam.res.setReplacement("com.google.android.googlequicksearchbox", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.google.android.googlequicksearchbox", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.slim.slimlauncher") != 0) {
+            resparam.res.setReplacement("com.slim.slimlauncher", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+            resparam.res.setReplacement("com.slim.slimlauncher", "drawable", "ic_allapps_pressed", modRes.fwd(R.drawable.drawer_pressed));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.microsoft.launcher") != 0) {
+            resparam.res.setReplacement("com.microsoft.launcher", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+        } else if (resparam.res.getIdentifier("ic_allapps", "drawable", "com.blackberry.blackberrylauncher") != 0) {
+            resparam.res.setReplacement("com.blackberry.blackberrylauncher", "drawable", "ic_allapps", modRes.fwd(R.drawable.drawer));
+        }
+    }
+}
